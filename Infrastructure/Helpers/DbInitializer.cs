8c20eb4a7fb7d180c6f693f576f324b949e627ca
UMMANCODE <ummanmm@code.edu.az>
2024-11-23 22:43:26 +0400

    Pipeline updated

--- Infrastructure/Helpers/DbInitializer.cs
+++ Infrastructure/Helpers/DbInitializer.cs
@@ -6,21 +6,25 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Helpers;
 
-public static class DbInitializer {
-  public static async Task SeedData(IServiceProvider serviceProvider) {
+public static class DbInitializer
+{
+  public static void SeedData(IServiceProvider serviceProvider)
+  {
     var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
     var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
     var env = serviceProvider.GetRequiredService<IWebHostEnvironment>();
     var context = serviceProvider.GetRequiredService<AppDbContext>();
   }
 
-  private static async Task<List<T>> ReadData<T>(IWebHostEnvironment env, string filePath) {
+  private static async Task<List<T>> ReadData<T>(IWebHostEnvironment env, string filePath)
+  {
     var fullFilePath = Path.Combine(env.WebRootPath, "data", filePath);
     if (!File.Exists(fullFilePath)) return [];
 
     var json = await File.ReadAllTextAsync(fullFilePath);
 
-    var data = JsonSerializer.Deserialize<List<T>>(json, new JsonSerializerOptions {
+    var data = JsonSerializer.Deserialize<List<T>>(json, new JsonSerializerOptions
+    {
       PropertyNameCaseInsensitive = true
     });
 
